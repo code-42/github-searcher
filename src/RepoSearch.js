e8874420fa17e3a01a5c9cf748550c8f65cdaaad
Edward Dupre <edwd42@gmail.com>
2018-10-19 16:55:18 -0400

    add file src/RepoSearch.js

---
+++ src/RepoSearch.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+function RepoSearch(props){
+    return(
+        <div>
+            <h1>Repo Search</h1>
+        </div>
+    );
+}
+
+export default connect()(RepoSearch);
